44adb869a0aa5ad805503a6ecd469e9d62abed53
atrzewik <42387362+atrzewik@users.noreply.github.com>
2018-11-07 19:44:52 +0100

    Add enter after 'and'

--- src/main/java/com/trzewik/blackjack/game/MessageProvider.java
+++ src/main/java/com/trzewik/blackjack/game/MessageProvider.java
@@ -21,15 +21,15 @@ public class MessageProvider {
 
     public static String getTellPlayerHandPointsBet = "You have %s, %s points and your bet value now is equal: %s ";
 
-    public static String winners = "The winner is %s with %s points and %s $prize - his cash capital is equal %s $, and";
+    public static String winners = "The winner is %s with %s points and %s $prize - his cash capital is equal %s $, and ";
 
     public static String winner = "The winner is %s with %s points and %s $prize - his cash capital is equal %s $";
 
-    public static String players = "%s with %s points and cash capital equal %s $, and";
+    public static String players = "%s with %s points and cash capital equal %s $, and ";
 
     public static String player = "%s with %s points and cash capital equal %s $";
 
-    public static String busters = "The busters is %s with %s points and cash capital equal %s $, and";
+    public static String busters = "The busters is %s with %s points and cash capital equal %s $, and ";
 
     public static String buster = "The busters is %s with %s points and cash capital equal %s $";
 
